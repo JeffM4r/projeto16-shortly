ff8977d5758ad1fd91de00053273d68e2d14b8bd
JefMartins <jeffersonmartins.jef@gmail.com>
2022-10-14 14:05:05 -0300

    criado arquivo inicial server.js para começar

---
+++ src/server.js
@@ -0,0 +1,13 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+const server = express();
+
+server.use(cors());
+server.use(express.json());
+
+server.listen(process.env.PORT,() =>{
+    console.log("listen on port " + process.env.PORT);
+})
